4930579de2337a9b13d4a1a070df7628664be5cb
Rakesh <rakesh.kumar.it26@heritageit.edu.in>
2025-06-18 10:29:23 +0530

    add npm run dev

--- config/mongoConfig.js
+++ config/mongoConfig.js
@@ -4,8 +4,9 @@ const connectToDatabase = async () => {
   const MONGO_URL =
     process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/mentorConnect20";
   try {
-    await mongoose.connect(MONGO_URL); // Removed deprecated options
-    console.log("Connected to MongoDB");
+    await mongoose.connect(MONGO_URL); 
+
+    console.log("Connected to MongoDB and mongo_URL is:", MONGO_URL);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1); // Exit the application if the database connection fails
